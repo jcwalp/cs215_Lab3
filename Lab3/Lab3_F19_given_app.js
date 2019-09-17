376c060f504906b33f68c10d279f1727c4b6f6e5
jon <43426006+jcwalp@users.noreply.github.com>
2019-09-17 10:22:25 -0400

    Add files via upload

---
+++ Lab3/Lab3_F19_given_app.js
@@ -0,0 +1,41 @@
+
+
+function logIn() {
+
+	/* 
+	this function will promtpt the user for their name; 
+	store name in a variable called userName.
+	Use variable called phrase, which will hold h2 content
+	Lastly write the "new" h2.
+	*/
+
+}//end function logIn()
+
+
+// create (global!) variable called numImages to hold total number of images;
+// create new array to hold preload images; call this array imageArray
+
+//use for loop to populate imageArray
+for (var i = 0; i < numImages; i++) {
+    //set image src property to image path, preloading image in the process
+
+}
+
+
+var i4_circleThru = 0; // global variable ( be careful) use for the function CicleThru()
+
+function circleThru() {
+
+    //if browser does not support the image object, exit.
+   
+    //  write images, from imageArray to HTML doc
+    
+    // call the setTimeout method on circleThru
+
+}//end circleThru()
+
+
+// call circleThru()
+
+
+
